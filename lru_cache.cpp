453d5ec982e99574d35fb6319e4cb57bd305c74d
k0fuji <fujimoto@cvl.iis.u-tokyo.ac.jp>
2015-06-29 22:23:38 +0900

    複数スレッドでLRUCacheを使用しthread safe に動作することを確認。以後画像の読み込みに対応するようにする。

--- lru_cache.cpp
+++ lru_cache.cpp
@@ -30,6 +30,7 @@ public:
     //キャッシュへの挿入
     void insert(std::string key, int data){
         mutex.lock();
+        std::cout << "insert " << data << std::endl; 
         //新データの場合
         if (mCacheMap.count(key) == 0)
         {
@@ -72,43 +73,63 @@ public:
         }
         else
         {
-            return 0;
+            return -1;
         }
-    }
-
-    void move(std::string key){
-
     }
 };
 
-void test(LRUCache* lruCache){
-    lruCache->insert("one",1);
-    lruCache->insert("two",2);
-    lruCache->insert("three",3);
-    lruCache->insert("four",4);
-    lruCache->insert("five",5);
-    lruCache->insert("six",6);
-    lruCache->insert("seven",7);
-    lruCache->insert("eight",8);
-    lruCache->insert("nine",9);
-    lruCache->insert("ten",10);
-    std::cout << lruCache->get("one") << std::endl;
-    std::cout << lruCache->get("eleven")<< std::endl;
-    lruCache->insert("eleven",11);
-    std::cout << lruCache->get("two")<< std::endl;
-    std::cout << lruCache->get("eleven")<< std::endl;
-    lruCache->insert("twelve",12);
-    std::cout << lruCache->get("twelve")<< std::endl;
-    std::cout << lruCache->get("three")<< std::endl;
-    std::cout << lruCache->get("thirteen")<< std::endl;
+void test(LRUCache& lruCache){
+    lruCache.insert("one",1);
+    lruCache.insert("two",2);
+    lruCache.insert("three",3);
+    lruCache.insert("four",4);
+    lruCache.insert("five",5);
+    lruCache.insert("six",6);
+    lruCache.insert("seven",7);
+    lruCache.insert("eight",8);
+    lruCache.insert("nine",9);
+    lruCache.insert("ten",10);
+    std::cout << "check " << 1 << ":" << lruCache.get("one") << std::endl;
+    std::cout << "check " << 11 << ":" << lruCache.get("eleven")<< std::endl;
+    lruCache.insert("eleven",11);
+    std::cout << "check " << 2 << ":" << lruCache.get("two")<< std::endl;
+    std::cout << "check " << 11 << ":" << lruCache.get("eleven")<< std::endl;
+    lruCache.insert("twelve",12);
+    std::cout << "check " << 12 << ":" << lruCache.get("twelve")<< std::endl;
+    std::cout << "check " << 3 << ":" << lruCache.get("three")<< std::endl;
+    std::cout << "check " << 13 << ":" << lruCache.get("thirteen")<< std::endl;
     return;
 }
 
+void test2(LRUCache& lruCache){
+    lruCache.insert("1one",101);
+    lruCache.insert("1two",102);
+    lruCache.insert("1three",103);
+    lruCache.insert("1four",104);
+    lruCache.insert("1five",105);
+    lruCache.insert("1six",106);
+    lruCache.insert("1seven",107);
+    lruCache.insert("1eight",108);
+    lruCache.insert("1nine",109);
+    lruCache.insert("1ten",110);
+    std::cout << "check " << 101 << ":" << lruCache.get("1one") << std::endl;
+    std::cout << "check " << 111 << ":" << lruCache.get("1eleven")<< std::endl;
+    lruCache.insert("1eleven",111);
+    std::cout << "check " << 102 << ":" << lruCache.get("1two")<< std::endl;
+    std::cout << "check " << 111 << ":" << lruCache.get("1eleven")<< std::endl;
+    lruCache.insert("1twelve",112);
+    std::cout << "check " << 112 << ":" << lruCache.get("1twelve")<< std::endl;
+    std::cout << "check " << 103 << ":" << lruCache.get("1three")<< std::endl;
+    std::cout << "check " << 113 << ":" << lruCache.get("1thirteen")<< std::endl;
+    return;
+}
+
+
 int main(){
-    pthread_t th1,th2;
-    LRUCache* lruCache = new LRUCache(10);
-    std::thread thread1(test,lruCache);
-    std::thread thread2(test,lruCache);
+    LRUCache lruCache(10);
+    std::thread thread1(test,std::ref(lruCache));
+    std::thread thread2(test2,std::ref(lruCache));
     thread1.join();
     thread2.join();
+    std::cout << "check " << 8 << ":" << lruCache.get("eight") << std::endl;
 }
